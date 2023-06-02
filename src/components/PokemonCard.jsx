eec55941961e170959e91e74238beff0c467320f
Sara Espinal <saraespinal1@gmail.com>
2023-06-02 18:30:35 -0500

    add pokemon details

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,14 +2,14 @@ import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import { StarOutlined } from '@ant-design/icons';
 
-const PokemonCard = ({ name }) => {
+const PokemonCard = ({ name, image }) => {
   return (
     <Card
       title={name}
       cover={
         <img
-          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png'
-          alt='Ditto'
+          src={image}
+          alt={name}
         />
       }
       extra={<StarOutlined />}
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,18 +1,24 @@
-import { PokemonCard } from "./PokemonCard";
+import { PokemonCard } from './PokemonCard';
 import './PokemonList.css';
 
 const PokemonList = ({ pokemons }) => {
-    return (
-        <div className="PokemonList">
-            {pokemons.map((pokemon) => {
-                return (<PokemonCard name={pokemon.name} key={pokemon.name} />);
-            })}
-        </div>
-    );
+  return (
+    <div className='PokemonList'>
+      {pokemons.map((pokemon) => {
+        return (
+          <PokemonCard
+            name={pokemon.name}
+            key={pokemon.name}
+            image={pokemon.sprites.front_default}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 PokemonList.defaultProps = {
-    pokemons: Array(10).fill(''),
-}
+  pokemons: Array(10).fill(''),
+};
 
 export { PokemonList };
