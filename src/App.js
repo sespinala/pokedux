3254cb869f3acda086764b7d905fd1516fe4dd7d
Sara Espinal <38758432+sespinala@users.noreply.github.com>
2023-06-02 14:52:01 -0500

    add axios and fetch pokemons (#2)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
+import { useEffect, useState } from 'react';
 import { Col } from 'antd';
 import { Searcher } from './components/Searcher';
-import './App.css';
 import { PokemonList } from './components/PokemonList';
+import { getPokemon } from './api';
+import logo from './static/logo.svg';
+import './App.css';
 
 function App() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const pokemonsRes = await getPokemon();
+      setPokemons(pokemonsRes);
+    };
+
+    fetchPokemons();
+  }, []);
+
   return (
     <div className='App'>
-      <Col span={8} offset={8}>
+      <Col
+        span={4}
+        offset={10}
+      >
+        <img
+          src={logo}
+          alt='Pokedux'
+        />
+      </Col>
+      <Col
+        span={8}
+        offset={8}
+      >
         <Searcher />
       </Col>
-      <PokemonList />
+      <PokemonList pokemons={pokemons}/>
     </div>
   );
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,18 +2,21 @@ import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import { StarOutlined } from '@ant-design/icons';
 
-const PokemonCard = () => {
-    return (
-        <Card 
-            title="Ditto"
-            cover={<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png" alt="Ditto" />}
-            extra={<StarOutlined />}
-        >
-            <Meta 
-                description="fire, magic"
-            />
-        </Card>
-    );
+const PokemonCard = ({ name }) => {
+  return (
+    <Card
+      title={name}
+      cover={
+        <img
+          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/132.png'
+          alt='Ditto'
+        />
+      }
+      extra={<StarOutlined />}
+    >
+      <Meta description='fire, magic' />
+    </Card>
+  );
 };
 
 export { PokemonCard };
