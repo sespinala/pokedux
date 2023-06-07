3ade20e98900e5952ea9fd5a020438e25c83fb40
Sara Espinal <38758432+sespinala@users.noreply.github.com>
2023-06-07 16:46:03 -0500

    add search functionality (#13)
    Co-authored-by: Sara Espinal <sara.espinala@perficient.com>

--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -1,7 +1,22 @@
 import { Input } from 'antd';
+import { useDispatch } from 'react-redux';
+import { setSearchPokemonsText } from '../slices/dataSlice';
 
 const Searcher = () => {
-    return <Input.Search placeholder='Buscar... ' style={{marginBottom: 10}}/>
+  const dispatch = useDispatch();
+
+  const handleOnSearchValue = (value) => {
+    dispatch(setSearchPokemonsText(value));
+  };
+
+  return (
+    <Input.Search
+      placeholder='Buscar... '
+      style={{ marginBottom: 10 }}
+      onSearch={handleOnSearchValue}
+      allowClear
+    />
+  );
 };
 
 export { Searcher };
--- src/slices/dataSlice.js
+++ src/slices/dataSlice.js
@@ -4,6 +4,8 @@ import { setLoading } from './uiSlice';
 
 const initialState = {
   pokemons: [],
+  searchPokemonsText: '',
+  filteredPokemons: []
 };
 
 export const fetchPokemonsWithDetails = createAsyncThunk(
@@ -15,6 +17,21 @@ export const fetchPokemonsWithDetails = createAsyncThunk(
       pokemonsRes.map((pokemon) => getPokemonDetails(pokemon))
     );
     dispatch(setPokemons(pokemonsDetailed));
+    dispatch(setLoading(false));
+  }
+);
+
+export const fetchFilteredPokemons = createAsyncThunk(
+  '',
+  async (_, { dispatch, getState }) => {
+    dispatch(setLoading(true));
+    const searchPokemonsText = getState().data.searchPokemonsText;
+    const pokemons = getState().data.pokemons;
+    const textFilter = searchPokemonsText.toLowerCase();
+    const filteredPokemons = pokemons.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(textFilter)
+    );
+    dispatch(setFilteredPokemons(filteredPokemons));
     dispatch(setLoading(false));
   }
 );
@@ -37,9 +54,20 @@ export const dataSlice = createSlice({
         state.pokemons[currentPokemonIndex].favorite = !isFavorite;
       }
     },
+    setSearchPokemonsText: (state, action) => {
+      state.searchPokemonsText = action.payload;
+    },
+    setFilteredPokemons: (state, action) => {
+      state.filteredPokemons = action.payload;
+    },
   },
 });
 
-export const { setFavorite, setPokemons } = dataSlice.actions;
+export const {
+  setFavorite,
+  setPokemons,
+  setSearchPokemonsText,
+  setFilteredPokemons,
+} = dataSlice.actions;
 
 export default dataSlice.reducer;
